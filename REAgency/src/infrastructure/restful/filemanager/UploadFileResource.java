6d9e38b7ff2fb171413753d25a46e9761c4dfdeb
masoud.b <masoud.b@gmail.com>
2015-03-28 17:57:32 -0400

    Country Service

--- REAgency/src/infrastructure/restful/filemanager/UploadFileResource.java
+++ REAgency/src/infrastructure/restful/filemanager/UploadFileResource.java
@@ -1,9 +1,14 @@
 package infrastructure.restful.filemanager;
 
+import infrastructure.hib.DIConfiguration;
+import infrastructure.hib.UnitOfWork;
+
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 
 import javax.ws.rs.Consumes;
@@ -17,59 +22,89 @@ import javax.ws.rs.core.Response.ResponseBuilder;
 
 import org.glassfish.jersey.media.multipart.FormDataContentDisposition;
 import org.glassfish.jersey.media.multipart.FormDataParam;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import core.contract.infracontract.IUnitOfWork;
+import core.domain.realestate.areaaggregate.Country;
 
 @Path("/file")
 public class UploadFileResource {
-	 
-		private static final String FILE_PATH = "D:/uploaded/xx.jpg";
-	 
-		@GET
-		@Path("/get")
-		@Produces("image/jpeg")
-		public Response getFile() {
-	 
-			File file = new File(FILE_PATH);
-	 
-			ResponseBuilder response = Response.ok((Object) file);
-			response.header("Content-Disposition",
+
+	private static final String FILE_PATH = "C:/uploaded/xx.jpg";
+
+	@GET
+	@Path("/get")
+	@Produces("image/jpeg")
+	public Response getFile() {
+
+		File file = new File(FILE_PATH);
+
+		ResponseBuilder response = Response.ok((Object) file);
+		response.header("Content-Disposition",
 				"attachment; filename=image_from_server.jpg");
-			return response.build();
-	 
+		return response.build();
+
+	}
+
+	@POST
+	@Path("/upload")
+	@Consumes(MediaType.MULTIPART_FORM_DATA)
+	public Response uploadFile(
+			@FormDataParam("file") InputStream uploadedInputStream,
+			@FormDataParam("file") FormDataContentDisposition fileDetail)
+			throws IOException {
+
+		String uploadedFileLocation = "d://uploaded/"
+				+ fileDetail.getFileName();
+		writeToFile(uploadedInputStream, uploadedFileLocation);
+		String output = "File uploaded to : " + uploadedFileLocation;
+		return Response.status(200).entity(output).build();
+	}
+
+	// save uploaded file to new location
+	private void writeToFile(InputStream uploadedInputStream,
+			String uploadedFileLocation) {
+
+		try {
+			OutputStream out = new FileOutputStream(new File(
+					uploadedFileLocation));
+			int read = 0;
+			byte[] bytes = new byte[1024];
+
+			out = new FileOutputStream(new File(uploadedFileLocation));
+			while ((read = uploadedInputStream.read(bytes)) != -1) {
+				out.write(bytes, 0, read);
+			}
+			out.flush();
+			out.close();
+		} catch (IOException e) {
+
+			e.printStackTrace();
 		}
-		@POST
-		@Path("/upload")
+
+	}
+
+	@POST
+		@Path("/countries")
 		@Consumes(MediaType.MULTIPART_FORM_DATA)
-		public Response uploadFile(
+		public String uploadCountriesFile(
 				@FormDataParam("file") InputStream uploadedInputStream,
 				@FormDataParam("file") FormDataContentDisposition fileDetail) throws IOException {
 			
-			String uploadedFileLocation = "d://uploaded/" + fileDetail.getFileName();
-			writeToFile(uploadedInputStream, uploadedFileLocation);
-			String output = "File uploaded to : " + uploadedFileLocation;
-			return Response.status(200).entity(output).build();
+			AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(
+				DIConfiguration.class);
+			BufferedReader reader = new BufferedReader(new InputStreamReader(uploadedInputStream));
+		    String line;
+		    while ((line = reader.readLine()) != null) {
+		    	
+				IUnitOfWork uow = context.getBean(UnitOfWork.class);
+		    	Country country = new Country();
+				country.setIsArchived(false);
+				country.setName(line);
+				uow.getCountryRepository().save(country);
+				uow.commit();
+		    }
+			context.close();
+			return (new Boolean(true).toString());
 		}
-	 
-		// save uploaded file to new location
-		private void writeToFile(InputStream uploadedInputStream,
-			String uploadedFileLocation) {
-	 
-			try {
-				OutputStream out = new FileOutputStream(new File(
-						uploadedFileLocation));
-				int read = 0;
-				byte[] bytes = new byte[1024];
-	 
-				out = new FileOutputStream(new File(uploadedFileLocation));
-				while ((read = uploadedInputStream.read(bytes)) != -1) {
-					out.write(bytes, 0, read);
-				}
-				out.flush();
-				out.close();
-			} catch (IOException e) {
-	 
-				e.printStackTrace();
-			}
-	 
-		}
-	 
-	}
+}
